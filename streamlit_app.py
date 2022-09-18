6091ab7192d8c458e69f1c18211c0a9a7bb440ab
Jeffrey Ng <jeffreyng@outlook.com>
2022-09-18 15:45:11 +0200

    Update streamlit_app.py
    fix broken meal

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free—Range Egg')
-streamlit.text('🥑🍞Avocado Toast)
+streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
