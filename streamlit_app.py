b3d1ddfe4b98938eb3b330299dcc99a2ebfdb236
Jeffrey Ng <jeffreyng@outlook.com>
2022-09-18 22:11:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+
+# write your own comment -what does the next line do? magic
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do? more magic
+streamlit.dataframe(fruityvice_normalized)
